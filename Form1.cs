a91f7f1eeeee507c0c2e0534ce7d1628d8f824e7
Stefan Dimitrovski <dondragonfull12@users.noreply.github.com>
2020-05-15 00:19:23 +0200

    movements

--- Form1.cs
+++ Form1.cs
@@ -16,12 +16,27 @@ namespace CarRacer
         {
             InitializeComponent();
         }
-        int gamespeed = 5;
+        private int gamespeed = 5;
+
         private void timer1_Tick(object sender, EventArgs e)
         {
+            moveEnemy(gamespeed);
             moveline(gamespeed);
         }
-        void moveline(int speed)
+
+        private void moveEnemy(int speed)
+        {
+            if(enemy.Top >= 480)
+            {
+                enemy.Top = -75;
+            }
+            else
+            {
+                enemy.Top += speed;
+            }
+        }
+
+        private void moveline(int speed)
         {
             if(pb1.Top >= 480)
             {
@@ -54,6 +69,28 @@ namespace CarRacer
             else
             {
                 pb4.Top += speed;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                if(player.Left > 15)
+                {
+                    player.Left -= 10;
+                }
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                if(player.Right < 350)
+                {
+                    player.Left += 10;
+                }
+            }
+            if(e.KeyCode == Keys.Escape)
+            {
+                this.Close();
             }
         }
     }
