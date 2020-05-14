5d259379580374b437fa41a4151c128af462ebee
Stefan Dimitrovski <dondragonfull12@users.noreply.github.com>
2020-05-14 03:01:08 +0200

    basic travel
    screen move illusion

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,21 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pb4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(-5, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(12, 443);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(359, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(12, 443);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pb1
+            // 
+            this.pb1.BackColor = System.Drawing.Color.White;
+            this.pb1.Location = new System.Drawing.Point(170, 0);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(7, 93);
+            this.pb1.TabIndex = 2;
+            this.pb1.TabStop = false;
+            // 
+            // pb2
+            // 
+            this.pb2.BackColor = System.Drawing.Color.White;
+            this.pb2.Location = new System.Drawing.Point(170, 139);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(7, 93);
+            this.pb2.TabIndex = 3;
+            this.pb2.TabStop = false;
+            // 
+            // pb3
+            // 
+            this.pb3.BackColor = System.Drawing.Color.White;
+            this.pb3.Location = new System.Drawing.Point(170, 279);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(7, 93);
+            this.pb3.TabIndex = 4;
+            this.pb3.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pb4
+            // 
+            this.pb4.BackColor = System.Drawing.Color.White;
+            this.pb4.Location = new System.Drawing.Point(170, 419);
+            this.pb4.Name = "pb4";
+            this.pb4.Size = new System.Drawing.Size(7, 93);
+            this.pb4.TabIndex = 5;
+            this.pb4.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
             this.ClientSize = new System.Drawing.Size(364, 441);
+            this.Controls.Add(this.pb4);
+            this.Controls.Add(this.pb3);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.pb1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximumSize = new System.Drawing.Size(380, 480);
+            this.MinimumSize = new System.Drawing.Size(380, 480);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Super Racer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pb4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,45 @@ namespace CarRacer
         {
             InitializeComponent();
         }
+        int gamespeed = 5;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            moveline(gamespeed);
+        }
+        void moveline(int speed)
+        {
+            if(pb1.Top >= 480)
+            {
+                pb1.Top = -75;
+            }
+            else
+            {
+                pb1.Top += speed;
+            }
+            if (pb2.Top >= 480)
+            {
+                pb2.Top = -75;
+            }
+            else
+            {
+                pb2.Top += speed;
+            }
+            if (pb3.Top >= 480)
+            {
+                pb3.Top = -75;
+            }
+            else
+            {
+                pb3.Top += speed;
+            }
+            if (pb4.Top >= 480)
+            {
+                pb4.Top = -75;
+            }
+            else
+            {
+                pb4.Top += speed;
+            }
+        }
     }
 }
